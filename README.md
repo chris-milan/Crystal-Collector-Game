e7c36d7a75de43cdb5b621309f855e4d12853c91
atmas <christopherjmilan@gmail.com>
2018-06-27 01:11:45 -0500

    finalizing assignment

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# unit-4-game
+Hello.
+
+This game generates a random number displayed in the center of the screen in red.
+You get to choose from 4 crystals that each contain a hidden and varied number value.
+Upon clicking a crystal, the hidden value will be added to the blue number. 
+In order to win, you must use the 4 crystals to add up to the sum of the red number.
+If your sum goes over the red number, you lose!
+After a win or loss, the hidden values in the crystal will randomly change, along with the red number.
---
+++ assets/javascript/game.js
@@ -0,0 +1,203 @@
+$(document).ready(function (){
+    
+
+//Fade-in Function
+$(function() {
+    $('mainContainer').hide().fadeIn(500);
+    $('h1').hide().fadeIn(1000);
+    $(".instructionsContainer" ).hide().fadeIn(2000);
+    $("#scoreContainer" ).hide().fadeIn(3000);
+    $("#randomNumber" ).hide().fadeIn(3500);
+    $("#winLossContainer" ).hide().fadeIn(4000);
+    $(".crystalsContainer" ).hide().fadeIn(4500);
+});
+
+//Random Computer Choices (from 19-120)
+var computerChoice = Math.floor(Math.random() * 102+19);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    
+// Win/Loss variables
+var wins = 0;
+var losses = 0;
+
+//Random Crystal Choices (from 1-12)
+var crystal1 = Math.ceil(Math.random() * 12);
+    console.log("Red Crystal = " + crystal1); 
+ 
+var crystal2 = Math.ceil(Math.random() * 12);
+    console.log("Blue Crystal = " + crystal2);
+
+var crystal3 = Math.ceil(Math.random() * 12);
+    console.log("Yellow Crystal = " + crystal3);
+    
+var crystal4 = Math.ceil(Math.random() * 12);
+    console.log("Green Crystal = " + crystal4); 
+    
+function crystalRandom() {
+var crystal1 = Math.ceil(Math.random() * 12);
+    console.log("Red Crystal = " + crystal1); 
+ 
+var crystal2 = Math.ceil(Math.random() * 12);
+    console.log("Blue Crystal = " + crystal2);
+
+var crystal3 = Math.ceil(Math.random() * 12);
+    console.log("Yellow Crystal = " + crystal3);
+    
+var crystal4 = Math.ceil(Math.random() * 12);
+    console.log("Green Crystal = " + crystal4);
+}
+
+var currentScore = 0;
+$("#current").text(currentScore);
+console.log(currentScore);
+
+
+    
+  $("#redCrystal").on("click", function () {
+    currentScore += crystal1;
+        $("#current").text(currentScore);
+            console.log(currentScore)
+      
+    if (currentScore === computerChoice) {
+        wins++; 
+        $("#wins").text("Wins: " + wins);
+        
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+    
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    } 
+      
+    else if (currentScore > computerChoice) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+        
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    }
+  })
+    
+    
+    
+ $("#blueCrystal").on("click", function () {
+    currentScore += crystal2;
+    $("#current").text(currentScore);
+      console.log(currentScore)
+     
+     if (currentScore === computerChoice) {
+        wins++;
+        $("#wins").text("Wins: " + wins);
+       
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+         
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    } 
+     
+    else if (currentScore > computerChoice) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+            
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+  }
+ })
+    
+    
+    
+ $("#yellowCrystal").on("click", function () {
+    currentScore += crystal3;
+    $("#current").text(currentScore);
+      console.log(currentScore)
+     if (currentScore === computerChoice) {
+        wins++;
+        $("#wins").text("Wins: " + wins);
+   
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+         
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    } 
+     
+    else if (currentScore > computerChoice) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+    
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+        
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);  
+  }   
+ })
+
+ $("#greenCrystal").on("click", function () {
+    currentScore += crystal4;
+    $("#current").text(currentScore);
+      console.log(currentScore)
+     if (currentScore === computerChoice) {
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+         
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+    }
+     
+     else if (currentScore > computerChoice) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        
+            crystalRandom();
+            totalScore = 0;
+            currentScore = 0;
+            $("#current").text(currentScore);
+    
+    computerChoice = Math.floor(Math.random() * 102+19);
+    $("#randomNumber" ).hide().fadeIn(2000);
+    $('#randomNumber').text(computerChoice);
+    console.log("Computer chose " + computerChoice);
+  }
+ }) 
+    
+});
